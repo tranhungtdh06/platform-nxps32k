0387227fc553d2ff6b7f744da69214124b3cd327
Hung <tranhungtdh06@gmail.com>
2026-06-20 23:12:43 +0700

    Sua loi duplicate linker script

--- builder/frameworks/baremetal.py
+++ builder/frameworks/baremetal.py
@@ -6,17 +6,19 @@ env = DefaultEnvironment()
 # Lấy đường dẫn của kho framework tải từ GitHub
 FRAMEWORK_DIR = env.PioPlatform().get_package_dir("framework-s32k144-baremetal")
 
-# Tự động nạp thư mục Include và file Linker
+# 1. Tự động nạp thư mục Include
 env.Append(
     CPPPATH=[
         os.path.join(FRAMEWORK_DIR, "include")
-    ],
-    LINKFLAGS=[
-        "-T", os.path.join(FRAMEWORK_DIR, "linker", "S32K144_64_flash.ld")
     ]
 )
 
-# Tự động biên dịch các file trong thư mục src của framework
+# 2. Báo cho PlatformIO biết vị trí file Linker (Cách chuẩn, không bị nhân đôi)
+env.Replace(
+    LDSCRIPT_PATH=os.path.join(FRAMEWORK_DIR, "linker", "S32K144_64_flash.ld")
+)
+
+# 3. Tự động biên dịch các file trong thư mục src của framework
 env.BuildSources(
     os.path.join("$BUILD_DIR", "FrameworkBaremetal"),
     os.path.join(FRAMEWORK_DIR, "src")
